8cb6202ddec9ea2ed50d349b840ae3aae6e36101
G <gauthier.castro@cfms.org.uk>
2021-05-04 11:20:11 +0100

    add logger_config.yaml

--- app.py
+++ app.py
@@ -1,31 +1,13 @@
 from flask import Flask, request, abort
 from logging.config import dictConfig
 
-dictConfig(
-    {
-        "version": 1,
-        "formatters": {
-            "default": {
-                "format": "[%(asctime)s] %(levelname)s in %(name)s of %(module)s : %(message)s",
-            }
-        },
-        "handlers": {
-            "wsgi": {
-                "class": "logging.StreamHandler",
-                "stream": "ext://flask.logging.wsgi_errors_stream",
-                "formatter": "default",
-            },
-            "logfile": {
-                "class": "logging.handlers.RotatingFileHandler",
-                "filename": "logconfig.log",
-                "maxBytes": 1024,
-                "backupCount": 3,
-                "formatter": "default",
-            },
-        },
-        "root": {"level": "INFO", "handlers": ["wsgi", "logfile"]},
-    }
-)
+
+import yaml
+
+with open("logger_config.yml") as f:
+    logger_config = yaml.safe_load(f)
+
+dictConfig(logger_config)
 
 app = Flask(__name__)
 
